cdbbff8cc6e8c5b4beef29025ded2202f1c4ca9b
Israelyan Leonid <leo.israelyan@icloud.com>
2022-04-13 19:21:47 +0300

    Initial Commit

---
+++ MarvelGram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelGram
+//
+//  Created by Леонид Исраелян on 13.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
